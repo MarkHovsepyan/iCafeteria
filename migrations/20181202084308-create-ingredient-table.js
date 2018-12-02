5b4ee618d59c261e7c0b66c2f7bb942506a4bb56
Mark <markhovsepyan98@gmail.com>
2018-12-02 13:13:22 +0400

    ident fix

--- migrations/20181202084308-create-ingredient-table.js
+++ migrations/20181202084308-create-ingredient-table.js
@@ -2,17 +2,17 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-      return queryInterface.createTable('Ingredient', {
-          id: {
-              type: Sequelize.INTEGER,
-              primaryKey: true,
-              autoIncrement: true,
-          },
-          name: Sequelize.STRING,
-      });
+    return queryInterface.createTable('Ingredient', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: Sequelize.STRING,
+    });
   },
 
   down: (queryInterface, Sequelize) => {
-      return queryInterface.dropTable('Ingredient');
+    return queryInterface.dropTable('Ingredient');
   }
 };
