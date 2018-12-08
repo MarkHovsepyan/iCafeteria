e4dc2e522402424e21bdf532149b44622542b841
Erik Davtyan <erikdavtyan@gmail.com>
2018-12-08 17:02:50 +0400

    Add foodIngredient associations, fix pk

--- models/foodIngredient.js
+++ models/foodIngredient.js
@@ -1,14 +1,21 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const foodIngredient = sequelize.define('foodIngredient', {
-    foodId: DataTypes.INTEGER,
-    ingredientId: DataTypes.INTEGER,
+    foodId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true
+    },
+    ingredientId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true
+    },
     quantity: DataTypes.INTEGER
   }, {
       freezeTableName: true
     });
   foodIngredient.associate = function (models) {
-    // associations can be defined here
+    models.foodIngredient.belongsTo(models.food, { foreignKey: 'foodId', targetKey: 'id', as: 'food' });
+    models.foodIngredient.belongsTo(models.food, { foreignKey: 'ingredientId', targetKey: 'id', as: 'ingredient' });
   };
   return foodIngredient;
 };
