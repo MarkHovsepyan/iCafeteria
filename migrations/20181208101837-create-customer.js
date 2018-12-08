aaa89ba02bbf7d8bdf7765f4418b2ec26fa96021
Erik Davtyan <erikdavtyan@gmail.com>
2018-12-08 16:12:53 +0400

    Change phoneNumber type to string

--- migrations/20181208101837-create-customer.js
+++ migrations/20181208101837-create-customer.js
@@ -17,7 +17,7 @@ module.exports = {
         type: Sequelize.STRING
       },
       phoneNumber: {
-        type: Sequelize.INTEGER
+        type: Sequelize.STRING
       },
       username: {
         allowNull: false,
