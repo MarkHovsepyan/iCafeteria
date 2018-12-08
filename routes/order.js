2357883d2718b2a5d06bbd7f18c844973b2fbdf6
Mark <markhovsepyan98@gmail.com>
2018-12-08 22:54:24 +0400

    [Routes] Added order route(initial version)

---
+++ routes/order.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const db = require('../models');
+
+const router = express.Router();
+
+router
+    .get('/', async (req, res) => {
+        const order = await db.order.findAll();
+        res.json({ order });
+    })
+    .get('/:id', async (req, res) => {
+        const order = await db.order.findByPk(req.params.id);
+        res.json({ order });
+    })
+    .post('/', async (req, res) => {
+        const { customerId } = req.body;
+        const order = await db.order.create({ customerId });
+        res.json({ order });
+    })
+    .delete('/:id', async (req, res) => {
+        const { id } = req.params;
+        try {
+            await db.category.destroy({ where: { id } });
+            res.json({ id });
+        } catch (e) {
+            res.json({ message: "error" });
+        }
+    });
+
+module.exports = router;
