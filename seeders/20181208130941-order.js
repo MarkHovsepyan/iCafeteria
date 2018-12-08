08178996c55bbb27a4205d99587ef40eab110129
Erik Davtyan <erikdavtyan@gmail.com>
2018-12-08 17:12:45 +0400

    Add order seeder

---
+++ seeders/20181208130941-order.js
@@ -0,0 +1,21 @@
+'use strict';
+const db = require('../models');
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const customers = await db.customer.findAll();
+    const getRandomCustomerId = () => customers[Math.floor(Math.random() * customers.length)].id;
+    const orders = new Array(100)
+      .fill(null)
+      .map(() => {
+        return {
+          customerId: getRandomCustomerId()
+        }
+      });
+    return db.order.bulkCreate(orders);
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('order');
+  }
+};
