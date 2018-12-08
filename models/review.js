1bd861e91d966411e4eb552c36dc17b6bc566a1d
Erik Davtyan <erikdavtyan@gmail.com>
2018-12-08 16:59:23 +0400

    Fix review model primary key

--- models/review.js
+++ models/review.js
@@ -1,16 +1,22 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const review = sequelize.define('review', {
+    customerId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true
+    },
+    foodId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true
+    },
     comment: DataTypes.STRING,
     star: DataTypes.INTEGER,
-    customerId: DataTypes.INTEGER,
-    foodId: DataTypes.INTEGER
   }, {
       freezeTableName: true
     });
   review.associate = function (models) {
-    models.review.belongsTo(models.customer);
-    models.review.belongsTo(models.food);
+    models.review.belongsTo(models.customer, { foreignKey: 'customerId', targetKey: 'id', as: 'customer' });
+    models.review.belongsTo(models.food), { foreignKey: 'foodId', targetKey: 'id', as: 'food' };
   };
   return review;
 };
