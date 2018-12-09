125493e07b7ea7193e36e7dc05353b394ccc315c
Erik Davtyan <erikdavtyan@gmail.com>
2018-12-09 15:57:28 +0400

    Add isVegan to food, add error handling

--- routes/food.js
+++ routes/food.js
@@ -5,28 +5,41 @@ const router = express.Router();
 
 router
   .get('/', async (req, res) => {
-    const foods = await db.food.findAll();
-    res.json({ foods });
+    try {
+      const foods = await db.food.findAll();
+      res.json({ foods });
+    } catch (e) {
+      res.json({ error: true });
+    }
   })
   .get('/byCategory/:categoryId*?', async (req, res) => {
-    const { categoryId } = req.params;
-    const where = categoryId == null ? {} : { categoryId };
-    const foodsRaw = await db.food.findAll({
-      where,
-      include: [{ model: db.category, as: 'category' }, { model: db.ingredient, as: 'ingredients' }],
-    });
-    const foods = foodsRaw
-      .map(food => food.get({ plain: true }))
-      .map(food => {
-        const { ingredients } = food;
-        const hasEnoughIngredients = !ingredients.some(i => i.quantity < i.foodIngredient.quantity);
-        return { ...food, hasEnoughIngredients };
+    try {
+      const { categoryId } = req.params;
+      const where = categoryId == null ? {} : { categoryId };
+      const foodsRaw = await db.food.findAll({
+        where,
+        include: [{ model: db.category, as: 'category' }, { model: db.ingredient, as: 'ingredients' }],
       });
-    res.json({ foods });
+      const foods = foodsRaw
+        .map(food => food.get({ plain: true }))
+        .map(food => {
+          const { ingredients } = food;
+          const hasEnoughIngredients = ingredients.every(i => i.quantity >= i.foodIngredient.quantity);
+          const isVegan = ingredients.every(i => i.vegan);
+          return { ...food, hasEnoughIngredients, isVegan };
+        });
+      res.json({ foods });
+    } catch (e) {
+      res.json({ error: true });
+    }
   })
   .get('/:id', async (req, res) => {
-    const food = await db.food.findByPk(req.params.id);
-    res.json({ food });
+    try {
+      const food = await db.food.findByPk(req.params.id);
+      res.json({ food });
+    } catch (e) {
+      res.json({ error: true });
+    }
   })
   .post('/', async (req, res) => {
     const { name } = req.body;
@@ -39,7 +52,7 @@ router
       await db.food.destroy({ where: { id } });
       res.json({ id });
     } catch (e) {
-      res.json({ message: 'error' });
+      res.json({ error: true });
     }
   });
 
