3004ec92d05b3b3b6756b6af1bf62a5fc6660cf6
Nelli <aghajanyannelli@gmail.com>
2018-12-09 16:30:01 +0400

    Added to ingredient and food seeders

--- seeders/20181208114538-food.js
+++ seeders/20181208114538-food.js
@@ -8,7 +8,14 @@ module.exports = {
       { name: 'Pepperoni', price: 1500, categoryId: 5 },
       { name: 'Tiramissu', price: 1000, categoryId: 2 },
       { name: 'Ceasar', price: 1500, categoryId: 1 },
-      { name: 'Long island iced tea', price: 3000, categoryId: 4 }
+      { name: 'Long island iced tea', price: 3000, categoryId: 4 },
+      { name: 'Mohito', price: 1500, categoryId: 4 },
+      { name: 'Napoleon', price: 800, categoryId: 2 },
+      { name: 'Diablo', price: 1500, categoryId: 5 },
+      { name: 'Pumpkin soup', price: 800, categoryId: 3 },
+      { name: 'Tomato soup', price: 700, categoryId: 3 },
+      { name: 'Spas', price: 600, categoryId: 3 },
+      { name: 'AUA', price: 1000, categoryId: 1 }
     ]);
   },
 
